178e2423f7a93af21a50f8c99b2ba416a1db113a
tavisca-agundawar <52607030+tavisca-agundawar@users.noreply.github.com>
2019-12-09 14:35:29 +0530

    Added project files

---
+++ The Rover/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace The_Rover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ The Rover/Rover.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_Rover
+{
+    public class Rover
+    {
+        public int MyProperty { get; set; }
+    }
+}
